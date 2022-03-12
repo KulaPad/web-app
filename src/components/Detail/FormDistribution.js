bef6f93b9dbbd0d1d1bedc3678a3dc64ea38ae42
Thiên Duy <duythien0912@gmail.com>
2022-03-12 14:32:14 +0700

    feat: add form whitelist, buy project

--- src/components/Detail/FormDistribution.js
+++ src/components/Detail/FormDistribution.js
@@ -1,32 +1,43 @@
 import { Box, Button } from "@chakra-ui/react";
 import moment from "moment";
 import React from "react";
-import { Link } from "react-router-dom";
 import Typography from "../KText";
 
 const FormDistribution = ({ project }) => {
   return (
-    <Box>
-      <Typography mt={2} type="text">
-        Whitelist open:{" "}
-        {project.whitelist_start_date &&
-          moment(+project.whitelist_start_date / 1000000)
+    <Box as={"form"}>
+      <Typography mt={1} type="text">
+        Distribution date:{" "}
+        {project.sale_end_date &&
+          moment(+project.sale_end_date / 1000000)
             .utc()
             .format("hh:mma DD/MM/YYYY")}
       </Typography>
-
-      <Link to={"/staking"} target="_blank" rel="noopener noreferrer">
-        <Button size="sm" colorScheme="blue" mt={1}>
-          Staking tokens and waiting to join
-        </Button>
-      </Link>
-      <Typography mt={1} type="caption">
-        *Required: Cost{" "}
-        <Box as="span" color="orange">
-          1 ticket
-        </Box>{" "}
-        to join this whitelist
+      <Typography mt={1} type="text">
+        Type: {project?.sale_type}
       </Typography>
+
+      <Typography mt={1} type="text">
+        Your token:{" "}
+        <Box fontWeight="bold" as="span" color="hotpink">
+          1000 {project?.token_symbol}{" "}
+        </Box>
+      </Typography>
+
+      <Button
+        fontFamily={"heading"}
+        mt={2}
+        size="sm"
+        // isDisabled={true}
+        bgGradient="linear(to-r, red.400,pink.400)"
+        color={"white"}
+        _hover={{
+          bgGradient: "linear(to-r, red.400,pink.400)",
+          boxShadow: "xl",
+        }}
+      >
+        Claim
+      </Button>
     </Box>
   );
 };
